107af0c200813d440037e605e37e22966a0e3c66
hanna lam <63362052+hannalam@users.noreply.github.com>
2025-01-25 23:49:59 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nicestaypro.com',
+  site: 'https://www.nicestaymanager.com',
   integrations: [
     tailwind(),
     sitemap({
